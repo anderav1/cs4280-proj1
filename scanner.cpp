0bd1730446424f26ce8fc0ae54a3eb3c5b470275
anderav1 <avabc5@umsystem.edu>
2022-03-05 17:49:10 -0600

    Changed to cpp

---
+++ scanner.cpp
@@ -0,0 +1,83 @@
+// Lexi Anderson
+//
+//
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "scanner.h"
+#include "token.h"
+
+
+const int fsaTable[10][15] = {
+          /* A-Z  a-z  0-9   *   WS    &    +    /    %    .    {    }    <    -  EOF */
+/* S0 */  {   6,   1,   5,   3,   0,   8,   8,   8,   8,   8,   8,   8,   9,  -1,1006 },
+/* S1 */  {   2,   2,   2,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1 },
+/* S2 */  {   2,   2,   2,1001,1001,1001,1001,1001,1001,1001,1001,1001,1001,1001,1001 },
+/* S3 */  {   3,   3,   3,   4,   3,   3,   3,   3,   3,   3,   3,   3,   3,   3,   3 },
+/* S4 */  {1002,1002,1002,1002,1002,1002,1002,1002,1002,1002,1002,1002,1002,1002,1002 },
+/* S5 */  {1003,1003,   5,1003,1003,1003,1003,1003,1003,1003,1003,1003,1003,1003,1003 },
+/* S6 */  {  -1,   7,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1 },
+/* S7 */  {1004,   7,1004,1004,1004,1004,1004,1004,1004,1004,1004,1004,1004,1004,1004 },
+/* S8 */  {1005,1005,1005,1005,1005,1005,1005,1005,1005,1005,1005,1005,1005,1005,1005 },
+/* S9 */  {  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,   8,   8,  -1 },
+};
+
+// getToken
+// Scan the next token
+token scan(FILE* fp, int line) {
+  token tkn;
+
+  int state = 0;
+  int nextState = 0;
+  
+  char* str = "";
+  char curr;
+  
+  int col;  // FSA table column
+  
+  // TODO: process chars until final state
+  while (state >= 0 && state < 1000) {
+    curr = getc(fp);
+    col = getTableCol(curr);
+    
+    if (col == -1) {  // invalid char
+      // TODO: return error
+      tkn.type = ERR_TK;
+      tkn.str = "Invalid token";
+      tkn.line = line;
+      
+      // TODO: print error message
+      return tkn;
+    }
+    
+    nextState = fsaTable[state][col];
+    if (nextState >= 1000 || nextState < 0) {  // final state
+      
+    } else {
+      
+    }
+  }
+}
+
+int getTableCol(char ch) {
+  if (isupper(ch) > 0) return 0;
+  if (islower(ch) > 0) return 1;
+  if (isdigit(ch) > 0) return 2;
+  if (ch == '*') return 3;
+  if (isspace(ch) > 0) return 4;
+  
+  if (ch == '&') return 5;
+  if (ch == '+') return 6;
+  if (ch == '/') return 7;
+  if (ch == '%') return 8;
+  if (ch == '.') return 9;
+  if (ch == '{') return 10;
+  if (ch == '}') return 11;
+  if (ch == '<') return 12;
+  if (ch == '-') return 13;
+  
+  if (ch == EOF) return 14;
+  
+  return -1;
+}
---
+++ token.h
@@ -0,0 +1,62 @@
+// Lexi Anderson
+//
+//
+
+#ifndef TOKEN_H
+#define TOKEN_H
+
+typedef enum tokenID {
+  ID_TK,
+  COMM_TK,
+  NUM_TK,
+  KEYWD_TK,
+  OP_TK,
+  EOF_TK,
+  ERR_TK
+};
+
+const char* tokenNames = [
+  "Identifier",
+  "Comment",
+  "Number",
+  "Keyword",
+  "Operator",
+  "EOF",
+  "Error"
+];
+
+// Respective int values for each final state
+// All final states < 0 or > 1000
+const int finalStates = [
+  1001,
+  1002,
+  1003,
+  1004,
+  1005,
+  1006,
+  -1
+];
+
+typedef struct {
+  tokenID type;
+  char* str;
+  int line;
+} token;
+
+const char* keywords = [
+  "Again",
+  "If", 
+  "Assign", 
+  "Move", 
+  "Show", 
+  "Flip", 
+  "Name", 
+  "Home", 
+  "Do", 
+  "Spot", 
+  "Place", 
+  "Here", 
+  "There"
+];
+
+#endif
